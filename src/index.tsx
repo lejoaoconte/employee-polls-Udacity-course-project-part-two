587d138f6e527656321ad30e1aca7dd1bcf0b102
João Conte <lejoaoconte@gmail.com>
2022-11-19 20:20:10 -0300

    Middlewares and initial data

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,30 @@ import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
 
-import { App } from "App";
+import App from "App";
 
 import { Default as Layout } from "layouts/Default";
 
 import { GlobalStyle } from "styles/GlobalStyles";
 
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import reducers from "redux/reducers";
+import middleware from "redux/middleware";
+
+const store = configureStore({ reducer: reducers, middleware: middleware });
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <Layout>
-      <App />
-      <GlobalStyle />
-    </Layout>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Layout>
+        <App />
+        <GlobalStyle />
+      </Layout>
+    </BrowserRouter>
+  </Provider>
 );
